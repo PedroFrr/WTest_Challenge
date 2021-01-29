1e18a6d7e7cee54243e393e3371eab813db16410
PedroFrr <pedroandref96@gmail.com>
2021-01-29 18:06:53 +0000

    Add BottomNavigation View

--- app/src/main/java/com/pedrofr/wtest/ui/MainActivity.kt
+++ app/src/main/java/com/pedrofr/wtest/ui/MainActivity.kt
@@ -2,13 +2,35 @@ package com.pedrofr.wtest.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.pedrofr.wtest.R
+import com.pedrofr.wtest.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setupBottomNavigation()
+
+
     }
+
+    private fun setupBottomNavigation()
+    {
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+
+
 }
